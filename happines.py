bce39bb90f5cff66533ea17f32c0b5b471d89f99
yeison serna <yeisonivanserna@gmail.com>
2022-11-09 14:46:38 -0500

    promedio paises csv

--- happines.py
+++ happines.py
@@ -1,7 +1,17 @@
 import csv
+import re
 
 # 1. Descargar el archivo happines.csv, abrirlo con python e imprimir todas las filas
 with open("happines.csv", newline="") as happinesFile:
     happinesData = csv.reader(happinesFile, delimiter=";")
+    suma = 0
+    contador = 0
+    # 3. Imprimir solamente la informacion de latinoamerica
     for row in happinesData:
-        print(row)
+        if row[1] == 'Latin America and Caribbean':
+            print(row)
+            suma = suma + float(row[3])
+            contador = contador + 1
+
+    promedio = suma / contador
+    print('promedio', promedio)
